cf4a264298a9b7f086567ad09b5d3d40e447e547
unknown <shaelappen@gmail.com>
2020-03-11 21:50:48 -0500

    Fixed problem 3

--- LinqProblems/LinqProblems/Program.cs
+++ LinqProblems/LinqProblems/Program.cs
@@ -35,7 +35,13 @@ namespace LinqProblems
 
 
             //Problem 3
-            Console.WriteLine("Problem 3");
+            //3.Using LINQ, write a function that calculates the class grade average after dropping the 
+            //lowest grade for each student.The function should take in a list of strings of grades 
+            //(e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, 
+            //averages the rest of the grades from that string, then averages the averages.
+
+
+          Console.WriteLine("Problem 3");
             List<string> classGrades = new List<string>()
             {
                 "80,100,92,89,65",
@@ -48,14 +54,22 @@ namespace LinqProblems
             List<double> averages = new List<double>();
             for (int i = 0; i < classGrades.Count; i++)
             {
-                averages.Add(classGrades[i].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades.Count).Select(n => n).Average());
+                averages.Add(classGrades[i].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades[i].Split(',').Length - 1).Average());
             }
             Console.WriteLine(averages.Average());
+
+
+
+
+
+
+
+
 
             //Breakdown below
             //List<double> averages = new List<double>();
 
-            //double result = classGrades[0].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades.Count).Select(n => n).Average();
+            //double result = classGrades[0].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).ToList().Take(classGrades[0].Split(',').Length - 1).Average();
             //double result2 = classGrades[1].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades.Count).Select(n => n).Average();
             //double result3 = classGrades[2].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades.Count).Select(n => n).Average();
             //double result4 = classGrades[3].Split(',').Select(n => Convert.ToDouble(n)).OrderByDescending(n => n).Take(classGrades.Count).Select(n => n).Average();
@@ -67,7 +81,6 @@ namespace LinqProblems
 
             //double final = averages.Average();
 
-            //Console.WriteLine(result);
             //Console.WriteLine(result2);
             //Console.WriteLine(result3);
             //Console.WriteLine(result4);
@@ -75,6 +88,8 @@ namespace LinqProblems
             //Console.WriteLine(final);
 
             Console.ReadLine();
+
+            
 
 
 
